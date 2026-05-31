8dfd809768fe136cf82a871b02e7e3737c35846c
Thanh Thư <2451160832-design@e.tlu.edu.vn>
2026-05-31 23:59:51 +0700

    [COMPONENT] Extract Hero component

---
+++ session_04_react/projects/portfolio_react/src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <section style={{ padding: '50px 20px', textAlign: 'center', background: '#f4f4f4' }}>
+            <h1>Chào tôi là lập trình viên React tương lai</h1>
+        </section>
+    );
+}
