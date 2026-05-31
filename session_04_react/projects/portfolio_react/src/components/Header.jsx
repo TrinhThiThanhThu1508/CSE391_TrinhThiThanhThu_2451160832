fca79107f7e11b6412a3a4b9eacf0cfd96da5680
Thanh Thư <2451160832-design@e.tlu.edu.vn>
2026-05-31 23:59:35 +0700

    [COMPONENT] Extract Header component

---
+++ session_04_react/projects/portfolio_react/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header style={{ padding: '20px', background: '#222', color: '#fff' }}>
+            <h2>My Portfolio</h2>
+        </header>
+    );
+}
